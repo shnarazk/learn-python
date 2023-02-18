2d40d137f2a547b1c3de7ea9689b3c7d4034611d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-02-18 11:26:00 +0900

    modified:   lesson05.py

--- lesson05.py
+++ lesson05.py
@@ -3,8 +3,9 @@ import random
 
 kuji: list[str] = ["lucky", "bad lack"]
 print(random.choice(kuji))
-
+## 身長(単位cm)
 h: float = float(input("?cm ")) / 100
+## 体重(単位kg)
 w: float = float(input("?kg "))
 bmi: float  = w / (h * h)
 print("your BMI is", bmi, ".")
