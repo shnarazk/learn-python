b1feb3c8be627c918eeb313305e19602ead5e9bf
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-02-19 20:43:54 +0900

    new file:   chapter4/p129-dispImage.py

---
+++ chapter4/p129-dispImage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image
+import PIL.ImageTk
+
+def dispPhoto(path):
+    newImage = PIL.Image.open(path).resize((300, 300))
+    imageData = PIL.ImageTk.PhotoImage(newImage)
+    imageLabel.configure(image = imageData)
+    imageLabel.image = imageData
+
+def openFile():
+    fpath = fd.askopenfilename()
+    if fpath:
+        dispPhoto(fpath)
+
+root = tk.Tk()
+root.geometry("300x300")
+btn = tk.Button(text = "open file", command = openFile)
+imageLabel = tk.Label()
+btn.pack()
+imageLabel.pack()
+tk.mainloop() 
