34157784355c3f32e3b8dde8be1af1999c29b934
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-03-10 15:35:32 +0900

    new file:   chapter3/loop-if1.py

---
+++ chapter3/loop-if1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+scores = [["あ", 68], ["い", 72], ["う", 85], ["え", 50], ["お", 100]]
+
+for score in scores:
+    if score[1] >= 60:
+        print(score[0])
+        print("OK")
+    else:
+        print("bad luck")
+    
